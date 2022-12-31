7015c993c4d610a0227c60e31a27e8549323aab9
Felipe Antônio Gomes de Mattos <96701867+fatzin@users.noreply.github.com>
2022-12-31 00:39:33 -0300

    Update merge_sorted_array.cpp

--- datastructure/merge_sorted_array.cpp
+++ datastructure/merge_sorted_array.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int max_size = m + n;
-        int curr_item;
         if(n > 0 && m > 0){
         nums1.resize(max_size-n);
         nums1.insert(nums1.end(), nums2.begin(), nums2.end());
